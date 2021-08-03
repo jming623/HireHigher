1cfd50e40af337a1954d4e617fdce5cafdb3b1ed
jming623 <jming623@naver.com>
2021-08-03 14:27:11 +0900

    마이페이지 게시글불러오기

--- src/main/java/com/hirehigher/controller/USERQUESTION_CONSTANT.java
+++ src/main/java/com/hirehigher/controller/USERQUESTION_CONSTANT.java
@@ -4,4 +4,5 @@ package com.hirehigher.controller;
 public class USERQUESTION_CONSTANT {
 	
 	public static final String UPLOAD_PATH = "C:\\Users\\gntj3\\Desktop\\upload";
+	//public static final String UPLOAD_PATH = "C:\\Users\\PC\\Desktop\\MyWeb_exer\\TeamProject\\HireHigher\\src\\main\\webapp\\resources\\img\\upload";
 }
