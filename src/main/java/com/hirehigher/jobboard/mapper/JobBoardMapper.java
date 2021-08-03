d4c206218266971bd2efd6368fdaf095ec824325
jihyeokpark <jparkdev777@gmail.com>
2021-08-03 13:34:20 +0900

    20210803

--- src/main/java/com/hirehigher/jobboard/mapper/JobBoardMapper.java
+++ src/main/java/com/hirehigher/jobboard/mapper/JobBoardMapper.java
@@ -8,11 +8,20 @@ import com.hirehigher.util.JobCriteria;
 
 public interface JobBoardMapper {
 
+	//등록
+	public int regist1(JobBoardVO jobBoardVO);
+	public int regist2(JobBoardDetailVO jobBoardDetailVO);
+	
+	//수정
+	public int update1(JobBoardDetailVO jobBoardDetailVO);
+	public int update2(JobBoardVO jobBoardVO); 
+	
+	//삭제
+	public int delete1(int bno);
+	public int delete2(int bno); 
+	
 	public int getTotal(JobCriteria cri); //총 게시글 수
 	public ArrayList<JobBoardVO> getList(JobCriteria cri); //조회
 	public JobBoardDetailVO getDetail(int bno); //상세
-	public int update(JobBoardDetailVO jobBoardDetailVO); //업데이트
-	public int regist(JobBoardDetailVO jobBoardDetailVO); //등록
-	public int delete(int bno); //삭제
 	public int viewCount(int bno);//조회수
 }
